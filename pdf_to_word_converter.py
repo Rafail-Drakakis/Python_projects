facdbdec99d80d512462d479dd9040e4ba0bc8b7
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-04-17 19:21:10 +0200

    make the file cleaner with encapsul the code in a function

--- pdf_to_word_converter.py
+++ pdf_to_word_converter.py
@@ -1,14 +1,9 @@
-# Import the required module
 import pdf2docx
 
-# Prompt the user for the input file name
-pdf_path = input("Enter the name of the PDF file to convert (include the .pdf extension): ")
+def pdf_to_word():
+	pdf_path = input("Enter the name of the PDF file to convert (include the .pdf extension): ") # Prompt the user for the input file name
+	docx_path = pdf_path.replace(".pdf", ".docx")  # Generate the output file name based on the input file name
+	pdf2docx.parse(pdf_path, docx_path) # Convert the PDF to Word
+	print(f"Conversion complete. Output file saved as {docx_path}") #print a message
 
-# Generate the output file name based on the input file name
-docx_path = pdf_path.replace(".pdf", ".docx")
-
-# Convert the PDF to Word
-pdf2docx.parse(pdf_path, docx_path)
-
-print(f"Conversion complete. Output file saved as {docx_path}")
-
+pdf_to_word()
