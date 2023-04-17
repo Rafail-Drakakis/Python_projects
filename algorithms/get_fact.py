de2c67ebec624500f76ac496796c2ee4a8914e10
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-04-17 20:43:29 +0200

    make the main.py more clear

--- algorithms/get_fact.py
+++
@@ -1,9 +0,0 @@
-#get_fact.py
-def get_fact():
-    number = int(input("Enter a number: "))
-    url = "http://numbersapi.com/{}".format(number)
-    r = requests.get(url)
-    if r.status_code == 200:
-        print(r.text)
-    else:
-        print("An error occurred, code={}".format(r.status_code))
--- algorithms/mp3_downloader.py
+++ algorithms/mp3_downloader.py
@@ -3,7 +3,7 @@ import os
 import pytube
 import moviepy.editor as mp
 
-def download_video_menu():
+def download_mp3_menu():
     video_or_playlist = int(input('Enter \n1.To download a single video or \n2.To download a playlist: '))
     if video_or_playlist == 1:
         url = input('Enter the URL of the video: ')
