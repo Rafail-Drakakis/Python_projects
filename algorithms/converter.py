0119770dc1b8c8b913072ac17266ff50f2aa59d7
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-04-17 20:36:41 +0200

    make the main.py more clear

---
+++ algorithms/converter.py
@@ -0,0 +1,40 @@
+from pdf2docx import parse
+from pydub import AudioSegment
+
+#audio_converter.py
+def convert_audio_menu():
+  input_path = input("Enter the file path To the input audio file: ")
+  output_path = input("Enter the file path To the output audio file: ")
+
+def convert_audio(input_file: str, output_file: str) -> None:
+    # Get the input file extension
+    input_file_ext = input_file.split(".")[-1]
+
+    # Get the output file extension
+    output_file_ext = output_file.split(".")[-1]
+
+    # Load audio file
+    audio = AudioSegment.from_file(input_file, format=input_file_ext)
+
+    # Export audio file
+    audio.export(output_file, format=output_file_ext)
+
+#pdf_to_word_converter.py    
+def pdf_to_word():
+	# Prompt the user for the input file name
+	pdf_path = input("Enter the name of the PDF file to convert (include the .pdf extension): ") 
+	
+	# Generate the output file name based on the input file name
+	docx_path = pdf_path.replace(".pdf", ".docx")  
+	
+	# Convert the PDF to Word
+	parse(pdf_path, docx_path) 
+	
+	print(f"Conversion complete. Output file saved as {docx_path}")
+
+def converter_menu():
+    choice = int(input("Enter\n 1.For convert an audio file \n2.For convert a PDF to word: "))
+    if choice == 1:
+        convert_audio_menu()
+    elif choice == 2:
+        pdf_to_word()
--- algorithms/main.py
+++ algorithms/main.py
@@ -1,4 +1,4 @@
-from audio_converter import convert_audio_menu
+from converter import converter_menu
 from count_text import count_text_menu
 from get_fact import get_fact
 from collatz_fibonacci import collatz_fibonacci_menu
@@ -6,9 +6,9 @@ from image_module import image_processing_menu
 from mp3_downloader import download_video_menu
 
 def main():
-    choice = int(input("Enter \n1.To convert an audio file  \n2.To count lines or unique words in a file \n3.To get a fact for a number \n4.To Generate a sequence \n5.To perform operations in an image \n6.To download videos as mp3: "))
+    choice = int(input("Enter \n1.To convert files to other formats  \n2.To count lines or unique words in a file \n3.To get a fact for a number \n4.To Generate a sequence \n5.To perform operations in an image \n6.To download videos as mp3: "))
     if choice == 1:
-        convert_audio_menu()
+        converter_menu()
     elif choice == 2:
         count_text_menu()
     elif choice == 3:
