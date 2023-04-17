3f4130fc520180c65651453632d551bf1c373a3d
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-04-17 19:42:31 +0200

    make the code more clear

--- pdf_to_word_converter.py
+++ pdf_to_word_converter.py
@@ -1,9 +1,15 @@
-import pdf2docx
+from pdf2docx import parse
 
 def pdf_to_word():
-	pdf_path = input("Enter the name of the PDF file to convert (include the .pdf extension): ") # Prompt the user for the input file name
-	docx_path = pdf_path.replace(".pdf", ".docx")  # Generate the output file name based on the input file name
-	pdf2docx.parse(pdf_path, docx_path) # Convert the PDF to Word
-	print(f"Conversion complete. Output file saved as {docx_path}") #print a message
+	# Prompt the user for the input file name
+	pdf_path = input("Enter the name of the PDF file to convert (include the .pdf extension): ") 
+	
+	# Generate the output file name based on the input file name
+	docx_path = pdf_path.replace(".pdf", ".docx")  
+	
+	# Convert the PDF to Word
+	parse(pdf_path, docx_path) 
+	
+	print(f"Conversion complete. Output file saved as {docx_path}")
 
 pdf_to_word()
