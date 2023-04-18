d4de2345d463d51041fd438dd29f66d07f7579fa
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-04-18 23:02:31 +0300

    add comments to the code to be more usable and user-friendly

--- projects/test.py
+++ projects/test.py
@@ -27,4 +27,5 @@ import os
 os.remove("sample.docx")
 os.remove("Hayko - Yerevani Gishernerum.mp3")
 os.remove("Песня День Победы - Лев Лещенко [9 мая] HD.mp3")
+os.remove("Ruben Haxverdyan - Im Pokrik Navak (Lyrics in description).mp3")
 os.remove("audio.wav")
