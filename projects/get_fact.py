533484b0e7cc515600233ef36385e55b2e9e9493
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-04-18 22:48:52 +0300

    add comments to the code to be more usable and user-friendly

--- projects/get_fact.py
+++ projects/get_fact.py
@@ -2,9 +2,14 @@
 import requests
 
 def get_fact(number):
+    # create a URL string by formatting the input number into the URL
     url = "http://numbersapi.com/{}".format(number)
+    
+    # send an HTTP GET request to the URL
     r = requests.get(url)
+    
+    # if the request is successful (status code 200), print the response text, otherwise, print an error message with the status code
     if r.status_code == 200:
         print(r.text)
-    else:
+    else: 
         print("An error occurred, code={}".format(r.status_code))
--- projects/mp3_downloader.py
+++ projects/mp3_downloader.py
@@ -5,23 +5,38 @@ import moviepy.editor as mp
 
 # function to download a single video as mp3
 def download_video_as_mp3(url): 
+    # create a YouTube object with the given URL
     youtube = YouTube(url)
+    # get the audio stream of the video
     video = youtube.streams.filter(only_audio=True).first()
+    # get the title of the video
     title = video.title
+    # print a message to show that the download is starting
     print(f'Downloading "{title}" as an mp3...')
+    # download the video
     video_path = video.download()
+    # create the output mp3 filename by changing the extension of the video filename
     mp3_path = os.path.splitext(video_path)[0] + '.mp3'
+    # create a MoviePy audio file clip from the video file
     clip = mp.AudioFileClip(video_path)
+    # write the audio clip to the output mp3 file
     clip.write_audiofile(mp3_path)
+    # close the clip object
     clip.close()
+    # delete the video file
     os.remove(video_path)
+    # print a message to show that the download is complete
     print(f'Download complete: "{title}.mp3"')
 
 # function to download a playlist as mp3
 def download_playlist_as_mp3(filename): 
+    # open the file that contains the URLs of the videos in the playlist
     with open(filename, 'r') as file:
+        # read the URLs into a list and remove any leading or trailing whitespace
         urls = file.readlines()
         urls = [url.strip() for url in urls]
+    # print a message to show how many URLs were found in the file
     print(f'Found {len(urls)} URLs in "{filename}"')
+    # download each video in the playlist
     for url in urls:
         download_video_as_mp3(url)
