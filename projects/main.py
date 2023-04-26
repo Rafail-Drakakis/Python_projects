c7c1daceed55bd214ac9dd880d2e0db3a75dfcc1
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-04-26 09:12:38 +0300

    clear the main file

---
+++ projects/main.py
@@ -0,0 +1,13 @@
+import collatz_fibonacci
+
+collatz_fibonacci.collatz_sequence(5)
+collatz_fibonacci.fibonacci_sequence(1, 5)
+
+import count_text
+
+count_text.count_lines("test.txt")
+count_text.count_words("test.txt")
+
+import get_fact
+
+get_fact.get_fact(5)
