bf7d3f38bcfb5afd1b195e256504e5d502b70a1d
Rafail Drakakis <rafaildrakakis123@gmail.com>
2023-05-17 09:06:36 +0300

    stylized main.py file

--- games/main.py
+++ games/main.py
@@ -4,7 +4,6 @@ from rock_paper_scissors import rock_paper_scissors
 from tic_tac_toe import tic_tac_toe
 from labyrinth import labyrinth
 
-#main.py
 def main():
     choice = int(input("Press \n1.To play tic tac toe \n2.To play hangman \n3.To play rock paper scissors \n4.To play the pong game \n5.To generate a random labyrinth: "))
     if choice == 1:
