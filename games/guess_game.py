772aa045f250f0b5f76794b3289e9db05758aff3
Rafail Drakakis <rafaildrakakis123@gmail.com>
2023-05-23 15:29:26 +0300

    add attempts limit to guessing game

--- games/guess_game.py
+++ games/guess_game.py
@@ -4,14 +4,15 @@ def guess_number():
     target_number = random.randint(1, 100)
     attempts = 0
 
-    while True:
+    while attempts < 10:
         guess = int(input("Guess a number between 1 and 100: "))
         attempts += 1
 
         if guess < target_number:
-            print("Too low!")
+            print("You guessed lower!")
         elif guess > target_number:
-            print("Too high!")
+            print("You guessed higher!")
         else:
             print(f"Congratulations! You guessed the number in {attempts} attempts.")
             break
+    print(f"Sorry, you've reached the maximum number of attempts. The number was {target_number}.")
