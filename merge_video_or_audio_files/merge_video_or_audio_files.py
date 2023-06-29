e2630271a2367e70bc222040f0074f89a4782a8b
Rafail Drakakis <rafaildrakakis123@gmail.com>
2023-06-29 20:44:08 +0300

    fixed choices option

--- merge_video_or_audio_files/merge_video_or_audio_files.py
+++ merge_video_or_audio_files/merge_video_or_audio_files.py
@@ -72,24 +72,18 @@ def collect_filenames(extension):
 # Main function
 def main():
     # Prompt the user to choose an option
-    choice = int(input("Choose an option:\n1. Enter filenames directly\n2. Specify a file to read filenames from\n"))
+    choice = int(input("Choose an option:\n1. Enter filenames directly\n2. Specify a file to read filenames from\nEnter 3 to include all files in this directory with a specified extension: "))
 
     if choice == 1:
         # Prompt the user to enter filenames directly
         files = [file.strip() for file in input("Enter the file names to merge (separated by commas): ").split(",")]
     elif choice == 2:
-        all_files = input("Do you want to include all files in this directory with a specified extension (yes/no)? ")
-
-        if all_files.lower() == "yes":
-            # Prompt the user to enter a file extension and collect filenames from the directory
-            extension = input("Enter the file extension (e.g., mp4, mp3, wav): ")
-            files = collect_filenames(extension)
-        else:
-            # Prompt the user to enter a filename containing the files
-            filename = input("Enter the filename which contains the files: ")
-            files = get_filenames_from_file(filename)
-
-    if os.path.exists("filenames.txt"):
+        # Prompt the user to enter a filename containing the files
+        filename = input("Enter the filename which contains the files: ")
+        files = get_filenames_from_file(filename)
+    elif choice == 3:
+        extension = input("Enter the file extension (e.g., mp4, mp3, wav): ")
+        files = collect_filenames(extension)
         os.remove("filenames.txt")
 
     if files:
