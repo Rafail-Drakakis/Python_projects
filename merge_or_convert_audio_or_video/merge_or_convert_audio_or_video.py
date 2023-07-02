9e12b23d1866a49441e98354c37290b065db7aa4
Rafail Drakakis <rafaildrakakis123@gmail.com>
2023-07-02 12:08:49 +0300

    improved code readability4

--- merge_or_convert_audio_or_video/merge_or_convert_audio_or_video.py
+++ merge_or_convert_audio_or_video/merge_or_convert_audio_or_video.py
@@ -1,12 +1,23 @@
-import os, glob, moviepy.editor, speech_recognition as sr
+import os
+import glob
+import moviepy.editor
+import speech_recognition as sr
 from moviepy.editor import VideoFileClip, AudioFileClip
 
-# Function to merge video or audio files
 def merge_video_or_audio_files(files, output_filename):
+    """
+    The function `merge_video_or_audio_files` takes a list of video or audio files, checks their
+    extensions, creates appropriate clips, concatenates them, and saves the merged file with the
+    specified output filename.
+    
+    :param files: A list of file paths to the video or audio files that you want to merge
+    :param output_filename: The output_filename parameter is a string that specifies the name of the
+    merged video or audio file that will be created
+    """
     clips = []
+    
     for file in files:
         try:
-            # Check the file extension and create appropriate clips
             if file.lower().endswith(('.mp4', '.mkv', '.avi')):
                 clip = moviepy.editor.VideoFileClip(file)
             elif file.lower().endswith(('.mp3', '.wav')):
@@ -14,49 +25,72 @@ def merge_video_or_audio_files(files, output_filename):
             else:
                 print(f"Skipping unsupported file: {file}")
                 continue
+            
             clips.append(clip)
+        
         except (FileNotFoundError, moviepy.editor.VideoFileClipException, moviepy.editor.AudioFileClipException) as e:
             print(f"Error processing file: {file} - {str(e)}")
 
     if clips:
         if isinstance(clips[0], moviepy.editor.VideoFileClip):
             try:
-                # Concatenate video clips and write the final merged video file
                 final_clip = moviepy.editor.concatenate_videoclips(clips, method='compose')
                 final_clip.write_videofile(output_filename)
                 print(f"Merged files saved as {output_filename}")
             except Exception as e:
                 print(f"Error writing video file: {str(e)}")
+        
         elif isinstance(clips[0], moviepy.editor.AudioFileClip):
             try:
-                # Concatenate audio clips and write the final merged audio file
                 final_clip = moviepy.editor.concatenate_audioclips(clips)
                 final_clip.write_audiofile(output_filename)
                 print(f"Merged files saved as {output_filename}")
             except Exception as e:
                 print(f"Error writing audio file: {str(e)}")
+    
     else:
         print("No supported files to merge.")
 
-# Function to read filenames from a file
 def get_filenames_from_file(filename):
+    """
+    The function `get_filenames_from_file` reads the contents of a file and returns a list of filenames,
+    with each filename on a separate line.
+    
+    :param filename: The filename parameter is a string that represents the name of the file from which
+    you want to retrieve the filenames
+    :return: The function `get_filenames_from_file` returns a list of filenames read from the specified
+    file. If the file is not found, it prints an error message and returns an empty list.
+    """
     try:
         with open(filename, 'r') as file:
             return file.read().splitlines()
+    
     except FileNotFoundError:
         print(f"File not found: {filename}")
         return []
 
-# Function to collect filenames with a specified extension from the current directory
 def collect_filenames(extension):
-    files = glob.glob(os.path.join(os.getcwd(), f'*.{extension}'))
+    """
+    The function `collect_filenames` collects filenames with a specific extension, prompts the user for
+    confirmation to proceed with merging the files, and writes the filenames to a text file.
+    
+    :param extension: The `extension` parameter is a string that represents the file extension of the
+    files you want to collect. For example, if you want to collect all files with the extension ".txt",
+    you would pass "txt" as the value for the `extension` parameter
+    :return: The function `collect_filenames` returns the list of filenames obtained from the file
+    "filenames.txt" using the `get_filenames_from_file` function.
+    """
+    files = glob.glob(os.path.join(os.getcwd(), f'*{extension}'))
+    
     if files:
         print("Collected files:")
         for file in files:
             print(file)
-        confirm = input("Do you want to proceed with merging these files (yes/no)? ")
+        
+        confirm = input("Do you want to proceed with these files (yes/no)? ")
+        
         if confirm.lower() != "yes":
-            print("Merge operation canceled.")
+            print("Operation canceled.")
             exit(0)
     else:
         print(f"No files with extension {extension} found.")
@@ -64,24 +98,31 @@ def collect_filenames(extension):
 
     with open('filenames.txt', 'w') as file:
         file.write('\n'.join(files))
+    
     return get_filenames_from_file("filenames.txt")
 
-# Function to convert video files to text
 def convert_video_to_text(input_filenames, read_from_file=False):
+    """
+    The function `convert_video_to_text` takes a list of input filenames, converts video or audio files
+    to text using speech recognition, and saves the transcriptions to text files.
+    
+    :param input_filenames: The `input_filenames` parameter is a list of filenames of the videos or
+    audio files that you want to convert to text. Each filename should be a string
+    :param read_from_file: The `read_from_file` parameter is a boolean flag that determines whether or
+    not to remove the original file(s) after converting them to text. If `read_from_file` is set to
+    `True`, the user will be prompted to confirm whether they want to remove the original file(s) after
+    conversion, defaults to False (optional)
+    """
     for filename in input_filenames:
         if filename.lower().endswith(('.mp4', '.mkv', '.avi', '.mp3', '.wav')):
             if filename.lower().endswith(('.mp4', '.mkv', '.avi')):
-                # Convert video to audio
                 video = VideoFileClip(filename)
                 audio = video.audio
             else:
-                # Use the audio file directly
                 audio = AudioFileClip(filename)
 
-            # Save audio to a temporary file
             audio.write_audiofile("temp.wav", codec='pcm_s16le')
 
-            # Convert audio to text
             recognizer = sr.Recognizer()
 
             with sr.AudioFile("temp.wav") as audio_file:
@@ -89,29 +130,39 @@ def convert_video_to_text(input_filenames, read_from_file=False):
 
             text = recognizer.recognize_google(audio)
 
-            # Save the transcriptions to a text file
             output_file_path = os.path.splitext(filename)[0] + ".txt"
+            
             with open(output_file_path, "w") as output_file:
                 output_file.write(text)
 
-            # Clean up temporary file
             os.remove("temp.wav")
 
-            if not read_from_file:
+            if read_from_file:
                 remove = input(f"Do you want to remove the original file '{filename}' (yes/no)? ")
+                
                 if remove == "yes":
                     os.remove(filename)
         else:
             print(f"Skipping unsupported file: {filename}")
 
-    if read_from_file:
+    if not read_from_file:
         remove = input("Do you want to remove the original file(s) (yes/no)? ")
+        
         if remove == "yes":
             for file in input_filenames:
                 os.remove(file)
 
-# Function to merge text files by extension
 def merge_files_by_extension(merged_file_name, extension):
+    """
+    The function `merge_files_by_extension` merges all files with a given extension into a single file.
+    
+    :param merged_file_name: The name of the merged file that will be created. This should be a string
+    without the file extension
+    :param extension: The "extension" parameter is a string that represents the file extension you want
+    to merge. For example, if you want to merge all files with the extension ".txt", you would pass
+    ".txt" as the value for the "extension" parameter
+    :return: the name of the merged file.
+    """
     merged_file = merged_file_name + extension
     files = [file for file in os.listdir() if file.endswith(extension)]
 
@@ -125,17 +176,19 @@ def merge_files_by_extension(merged_file_name, extension):
 
     return merged_file
 
-# Main function
 def main():
-    merge_or_convert = int(input("1. To merge video/audio\n2. To convert it to text: "))
+    """
+    The main function allows the user to either merge video/audio files or convert them to text, based
+    on their choice.
+    """
+    merge_or_convert = int(input("1. To merge video/audio\n2. To convert video/audio to text: "))
+    
     if merge_or_convert == 1:
         choice = int(input("Choose an option:\n1. Enter filenames directly\n2. Specify a file to read filenames from\n3. To include all files in this directory with a specified extension: "))
 
         if choice == 1:
-            # Prompt the user to enter filenames directly
             files = [file.strip() for file in input("Enter the file names to merge (separated by commas): ").split(",")]
         elif choice == 2:
-            # Prompt the user to enter a filename containing the files
             filename = input("Enter the filename which contains the files: ")
             files = get_filenames_from_file(filename)
         elif choice == 3:
@@ -144,7 +197,6 @@ def main():
             os.remove("filenames.txt")
 
         if files:
-            # Prompt the user to enter the output file name and merge the files
             output_filename = input("Enter the output file name: ")
             merge_video_or_audio_files(files, output_filename)
         else:
@@ -153,6 +205,7 @@ def main():
     
     elif merge_or_convert == 2:
         choice = int(input("Enter \n1 to collect filenames from the current directory\n2 to enter filenames manually: "))
+        
         if choice == 1:
             extension = input("Enter the extension you want: ")
             filenames = collect_filenames(extension)
@@ -166,6 +219,7 @@ def main():
         merged_file = merge_files_by_extension("merged", ".txt")
 
         remove_txt_files = input("Do you want to remove the text files after merging (yes/no)? ")
+        
         if remove_txt_files.lower() == "yes":
             for file in os.listdir():
                 if file.endswith(".txt") and file != merged_file:
