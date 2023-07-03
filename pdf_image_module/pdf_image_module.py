e1e61c88425401d614e22691919adbf4c33022d9
Rafail Drakakis <rafaildrakakis123@gmail.com>
2023-07-03 16:47:19 +0300

    added error handling

--- pdf_image_module/pdf_image_module.py
+++ pdf_image_module/pdf_image_module.py
@@ -6,103 +6,136 @@ def split_pdf(filename, page_ranges):
     """
     The `split_pdf` function takes a PDF file and a list of page ranges as input, and creates a new PDF
     file containing only the specified pages.
-    
+
     :param filename: The `filename` parameter is the name of the PDF file that you want to split. It
     should be a string representing the file path or file name of the PDF file
     :param page_ranges: The `page_ranges` parameter is a list of page ranges that specify which pages to
     extract from the PDF file. Each page range can be specified in one of the following formats:
     """
-    all_pages = []
-    for page_range in page_ranges:
-        if isinstance(page_range, int):
-            all_pages.append(page_range)
-        elif isinstance(page_range, str):
-            if '-' in page_range:
-                start, end = page_range.split('-')
-                if not start.isdigit() or not end.isdigit():
-                    raise ValueError(f'Invalid page range: {page_range}')
-                start = int(start)
-                end = int(end)
-                if start > end:
-                    raise ValueError(f'Invalid page range: {page_range}')
-                all_pages.extend(range(start, end + 1))
+    try:
+        all_pages = []
+        for page_range in page_ranges:
+            if isinstance(page_range, int):
+                all_pages.append(page_range)
+            elif isinstance(page_range, str):
+                if '-' in page_range:
+                    start, end = page_range.split('-')
+                    if not start.isdigit() or not end.isdigit():
+                        raise ValueError(f'Invalid page range: {page_range}')
+                    start = int(start)
+                    end = int(end)
+                    if start > end:
+                        raise ValueError(f'Invalid page range: {page_range}')
+                    all_pages.extend(range(start, end + 1))
+                else:
+                    if not page_range.isdigit():
+                        raise ValueError(f'Invalid page range: {page_range}')
+                    all_pages.append(int(page_range))
             else:
-                if not page_range.isdigit():
-                    raise ValueError(f'Invalid page range: {page_range}')
-                all_pages.append(int(page_range))
-        else:
-            raise ValueError(f'Invalid page range: {page_range}')
+                raise ValueError(f'Invalid page range: {page_range}')
 
-    with open(filename, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        num_pages = len(reader.pages)
-        if not all(1 <= p <= num_pages for p in all_pages):
-            raise ValueError('Invalid page range')
-        writer = PyPDF2.PdfWriter()
-        for p in all_pages:
-            writer.add_page(reader.pages[p - 1])
-        with open("new_file.pdf", 'wb') as new_file:
-            writer.write(new_file)
+        with open(filename, 'rb') as file:
+            reader = PyPDF2.PdfReader(file)
+            num_pages = len(reader.pages)
+            if not all(1 <= p <= num_pages for p in all_pages):
+                raise ValueError('Invalid page range')
+            writer = PyPDF2.PdfWriter()
+            for p in all_pages:
+                writer.add_page(reader.pages[p - 1])
+            with open("new_file.pdf", 'wb') as new_file:
+                writer.write(new_file)
+    
+    except FileNotFoundError as e:
+        print(f"Error: File not found. {e}")
+    except ValueError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def convert_pdf(*pdf_paths):
     """
     The `convert_pdf` function converts PDF files to either images or Word documents based on the user's
     input.
     """
-    conversion_option = input("Enter the conversion option (word/images): ")
+    try:
+        conversion_option = input("Enter the conversion option (word/images): ")
 
-    if conversion_option.lower() == "images":
-        for pdf_path in pdf_paths:
-            images = pdf2image.convert_from_path(pdf_path, dpi=1000)
-            for idx, img in enumerate(images):
-                img.save(f'page_{idx + 1}.jpg', 'JPEG', quality=80)
-    
-    elif conversion_option.lower() == "word":
-        docx_paths = []
-        for pdf_path in pdf_paths:
-            docx_path = pdf_path.replace(".pdf", ".docx")
-            pdf2docx.parse(pdf_path, docx_path)
-            docx_paths.append(docx_path)
+        if conversion_option.lower() == "images":
+            for pdf_path in pdf_paths:
+                try:
+                    images = pdf2image.convert_from_path(pdf_path, dpi=1000)
+                    for idx, img in enumerate(images):
+                        img.save(f'page_{idx + 1}.jpg', 'JPEG', quality=80)
+                except Exception as e:
+                    print(f"Error converting PDF to images: {e}")
 
-def merge_pdf_files(output_filename):
+        elif conversion_option.lower() == "word":
+            docx_paths = []
+            for pdf_path in pdf_paths:
+                try:
+                    docx_path = pdf_path.replace(".pdf", ".docx")
+                    pdf2docx.parse(pdf_path, docx_path)
+                    docx_paths.append(docx_path)
+                except Exception as e:
+                    print(f"Error converting PDF to Word: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def merge_pdf_files(output_filename, all_files=True):
     """
-    The function `merge_pdf_files` merges multiple PDF files into a single PDF file, and optionally
-    deletes the original files.
-    
-    :param output_filename: The `output_filename` parameter is the name of the merged PDF file that will
-    be created. It should be a string representing the desired name of the output file
+    The `merge_pdf_files` function merges multiple PDF files into a single PDF file, either by selecting
+    all files in the current directory or by manually entering the filenames.
+
+    :param output_filename: The output_filename parameter is a string that specifies the name of the
+    merged PDF file that will be created
+    :param all_files: The `all_files` parameter is a boolean flag that determines whether to merge all
+    PDF files in the current working directory or to manually enter the filenames to be merged, defaults
+    to True (optional)
     """
-    files = sorted(glob.glob(os.path.join(os.getcwd(), f'*.pdf')))
-    target_file = "filenames.txt"
+    try:
+        if all_files:
+            files = sorted(glob.glob(os.path.join(os.getcwd(), '*.pdf')))
+            if not files:
+                print("Error: No PDF files found in the current directory.")
+                return
 
-    with open(target_file, 'w') as file:
-        file.write('\n'.join(files))
+            merger = PyPDF2.PdfMerger()
+            for filename in files:
+                merger.append(filename)
+            merger.write(output_filename)
+            merger.close()
+        else:
+            filenames = []
+            while True:
+                try:
+                    filename = input("Enter the filename (or 'done' to finish): ")
+                    if filename == "done":
+                        break
+                    if not os.path.exists(filename):
+                        print(f"Error: File '{filename}' does not exist.")
+                        continue
+                    filenames.append(filename)
+                except Exception as e:
+                    print(f"An error occurred: {e}")
 
-    filenames = []
-    with open(target_file, 'r') as file:
-        filenames = file.read().splitlines()
+            if not filenames:
+                print("No files specified.")
+                return
 
-    merger = PyPDF2.PdfMerger()
-    for filename in filenames:
-        merger.append(filename)
-    merger.write(output_filename)
-    merger.close()
+            merger = PyPDF2.PdfMerger()
+            for filename in filenames:
+                merger.append(filename)
+            merger.write(output_filename)
+            merger.close()
 
-    delete = input("Do you want to delete the original files (yes/no): ")
-    if delete.lower() == "yes":
-        with open(target_file, 'r') as file:
-            for line in file:
-                filepath = line.strip()  # Remove newline character
-                os.remove(filepath)
-
-    if os.path.exists(target_file):
-        os.remove(target_file)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def pdf_to_excel(input_pdf_path, output_excel_path):
     """
     The function `pdf_to_excel` converts a PDF file into an Excel file by using the `tabula`
     library in Python.
-    
+
     :param input_pdf_path: The input_pdf_path parameter is the file path of the PDF file that you want
     to convert to Excel. It should be a string that specifies the location of the PDF file on your
     computer
@@ -111,11 +144,17 @@ def pdf_to_excel(input_pdf_path, output_excel_path):
     you want to save the file as `converted_data.xlsx` in the current directory, you can set
     `output_excel
     """
-    # Convert PDF to Excel
-    pdf_data_frames = tabula.read_pdf(input_pdf_path, pages='all')
-    with pd.ExcelWriter(output_excel_path, engine='openpyxl') as excel_writer:
-        for i, df in enumerate(pdf_data_frames):
-            df.to_excel(excel_writer, sheet_name=f"Sheet{i+1}", index=False)
+    try:
+        # Convert PDF to Excel
+        pdf_data_frames = tabula.read_pdf(input_pdf_path, pages='all')
+        with pd.ExcelWriter(output_excel_path, engine='openpyxl') as excel_writer:
+            for i, df in enumerate(pdf_data_frames):
+                df.to_excel(excel_writer, sheet_name=f"Sheet{i + 1}", index=False)
+    
+    except FileNotFoundError as e:
+        print(f"Error: File not found. {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def merge_excel_sheets(input_excel_path, output_excel_path):
     """
@@ -126,9 +165,14 @@ def merge_excel_sheets(input_excel_path, output_excel_path):
     :param output_excel_path: The path where the merged Excel sheets will be saved
     """
     # Merge Excel sheets
-    dfs = pd.read_excel(input_excel_path, sheet_name=None)
-    merged_data_frame = pd.concat(dfs.values(), ignore_index=True)
-    merged_data_frame.to_excel(output_excel_path, index=False)
+    try:
+        dfs = pd.read_excel(input_excel_path, sheet_name=None)
+        merged_data_frame = pd.concat(dfs.values(), ignore_index=True)
+        merged_data_frame.to_excel(output_excel_path, index=False)
+    except FileNotFoundError as e:
+        print(f"Error: File not found. {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def shift_empty_cells(output_excel_path):
     """
@@ -140,22 +184,27 @@ def shift_empty_cells(output_excel_path):
     you want to modify. It should be a string that specifies the location of the file on your computer
     """
     # Load output Excel file
-    excel_workbook = openpyxl.load_workbook(output_excel_path)
-    for worksheet_name in excel_workbook.sheetnames:
-        excel_worksheet = excel_workbook[worksheet_name]
-        for row in excel_worksheet.iter_rows():
-            empty_cell_list = []
-            for current_cell in row:
-                if current_cell.value is None:
-                    empty_cell_list.append(current_cell)
-                else:
-                    for empty_cell_to_fill in empty_cell_list:
-                        empty_cell_to_fill.value = current_cell.value
-                        current_cell.value = None
-                        empty_cell_list.remove(empty_cell_to_fill)
+    try:
+        excel_workbook = openpyxl.load_workbook(output_excel_path)
+        for worksheet_name in excel_workbook.sheetnames:
+            excel_worksheet = excel_workbook[worksheet_name]
+            for row in excel_worksheet.iter_rows():
+                empty_cell_list = []
+                for current_cell in row:
+                    if current_cell.value is None:
                         empty_cell_list.append(current_cell)
-                        break
-    excel_workbook.save(output_excel_path)
+                    else:
+                        for empty_cell_to_fill in empty_cell_list:
+                            empty_cell_to_fill.value = current_cell.value
+                            current_cell.value = None
+                            empty_cell_list.remove(empty_cell_to_fill)
+                            empty_cell_list.append(current_cell)
+                            break
+        excel_workbook.save(output_excel_path)
+    except FileNotFoundError as e:
+        print(f"Error: File not found. {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def convert_pdf_to_excel(input_pdf_path):
     """
@@ -274,15 +323,26 @@ def convert_image(input_path, output_format):
     "PNG", "BMP", etc
     :return: the output path of the converted image.
     """
-    if not os.path.exists(input_path):
-        return f"Error: file '{input_path}' does not exist."
-    output_dir = os.path.dirname(input_path)
-    output_filename = input("Enter the output filename: ")
-    output_path = os.path.join(output_dir, f"{output_filename}.{output_format.lower()}")
-    with PIL.Image.open(input_path) as im:
-        rgb_im = im.convert('RGB')
-        rgb_im.save(output_path, format=output_format.upper())
-    return output_path
+    try:
+        if not os.path.exists(input_path):
+            raise FileNotFoundError(f"Error: File '{input_path}' does not exist.")
+
+        output_dir = os.path.dirname(input_path)
+        output_filename = input("Enter the output filename: ")
+        output_path = os.path.join(output_dir, f"{output_filename}.{output_format.lower()}")
+
+        with PIL.Image.open(input_path) as im:
+            rgb_im = im.convert('RGB')
+            rgb_im.save(output_path, format=output_format.upper())
+
+        return output_path
+    
+    except FileNotFoundError as e:
+        return f"Error: {e}"
+    except PIL.UnidentifiedImageError as e:
+        return f"Error: Invalid image format. {e}"
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 def images_to_pdf(images, pdf_name):
     """
@@ -310,7 +370,7 @@ def menu():
     The `menu()` function displays a menu of options and performs different actions based on the user's
     choice.
     """
-    choice = int(input("1.Extract Images to Text\n2.Mirror Image\n3.Convert Image\n4.Convert Images to PDF\n5. Merge PDF Files\n6. Convert PDF to Images or Word\n7. Split PDF\n8. Convert PDF to Excel\nEnter your choice: "))
+    choice = int(input("1.Extract Images to Text\n2.Mirror Image\n3.Convert Image\n4.Convert Images to PDF\n5.Merge PDF Files\n6.Convert PDF to Images or Word\n7.Split PDF\n8.Convert PDF to Excel\nEnter your choice: "))
 
     if choice == 1:
         image_path = input("Enter the image path: ")
@@ -330,7 +390,11 @@ def menu():
         images_to_pdf(image_paths, pdf_name)
     elif choice == 5:
         output_filename = input("Enter the output PDF filename: ")
-        merge_pdf_files(output_filename)
+        all_files = int(input("Press 1 to specify the pdf files or 2 to merge all of them in the directory: "))
+        if all_files == 1:
+            merge_pdf_files(output_filename, all_files = False)
+        elif all_files == 2:
+            merge_pdf_files(output_filename, all_files = True)
     elif choice == 6:
         pdf_paths = input("Enter the PDF paths (comma-separated): ").split(",")
         convert_pdf(*pdf_paths)
